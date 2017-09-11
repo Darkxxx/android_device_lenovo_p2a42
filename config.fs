aaac824ab2dc0a46d190b0ea5185624963a1c4d0
Murat Kozan <muratkozan300@gmail.com>
2017-09-12 00:23:47 +0300

    Update config.fs

--- config.fs
+++ config.fs
@@ -84,15 +84,3 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
-
-[system/vendor/bin/slim_daemon]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
-
-[vendor/bin/slim_daemon]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
